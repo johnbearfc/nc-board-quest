816c1f171945cc294ca9795da8e669c2d78d8967
johnbearfc <jclleggett@gmail.com>
2021-09-20 12:36:45 +0100

    add seed for table creation

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,12 @@
 const db = require('../db/connection.js');
 const testData = require('../db/data/test-data/index.js');
-const { seed } = require('../db/seeds/seed.js');
+const seed = require('../db/seeds/seed.js');
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe('testing seed', () => {
+    test('should ', () => {
+        
+    });
+});
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,52 @@
-const seed = (data) => {
+const db = require('../connection.js');
+
+const seed = async (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
-  // 1. create tables
+
+  await db.query(`
+    DROP TABLE IF EXISTS comments;
+    DROP TABLE IF EXISTS reviews;
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS categories;`
+  );
+
+  await db.query(
+    `CREATE TABLE categories (
+      slug VARCHAR(50) PRIMARY KEY,
+      description TEXT NOT NULL
+    );`
+  );
+  await db.query(
+    `CREATE TABLE users (
+      username VARCHAR(50) PRIMARY KEY,
+      avatar_url TEXT,
+      name TEXT NOT NULL
+    );`
+  );
+  await db.query(
+    `CREATE TABLE reviews (
+      review_id SERIAL PRIMARY KEY,
+      title VARCHAR(80) NOT NULL,
+      review_body TEXT NOT NULL,
+      designer VARCHAR(50) NOT NULL,
+      review_img_url TEXT DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+      votes INT DEFAULT 0,
+      category VARCHAR(50) REFERENCES categories(slug) NOT NULL,
+      owner VARCHAR(50) references users(username) NOT NULL,
+      created_at DATE DEFAULT CURRENT_DATE
+    );`
+  );
+  await db.query(
+    `CREATE TABLE comments (
+      comment_id SERIAL PRIMARY KEY,
+      author VARCHAR(50) references users(username) NOT NULL,
+      review_id INT REFERENCES reviews(review_id) NOT NULL,
+      votes INT DEFAULT 0,
+      created_at DATE DEFAULT CURRENT_DATE,
+      body TEXT NOT NULL
+    );`
+  );
+
   // 2. insert data
 };
 
