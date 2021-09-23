206215d8f5822063c0d2307dcc4f4caad06fdc41
johnbearfc <jclleggett@gmail.com>
2021-09-23 17:09:46 +0100

    add routing for users

--- routers/api.route.js
+++ routers/api.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const categoriesRouter = require('./categories.route');
 const reviewsRouter = require('./reviews.route');
 const commentsRouter = require('./comments.route');
+const usersRouter = require('./users.route');
 
 const apiRouter = express.Router();
 
@@ -10,6 +11,8 @@ apiRouter.use('/categories', categoriesRouter);
 apiRouter.use('/reviews', reviewsRouter);
 
 apiRouter.use('/comments', commentsRouter);
+
+apiRouter.use('/users', usersRouter);
 
 
 module.exports = apiRouter;
